dbd95048e096038cd73981b5d23671a520c283aa
Nikody456 <antonnik00@mail.ru>
2025-03-24 00:26:17 +0300

    Initial commit

---
+++ FinanceTracker.API/Controllers/TransactionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransactionsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var transactions = new[]
+        {
+            new { Id = 1, Amount = 1000, Type = "Доход", Category = "Зарплата", Date = DateTime.Now },
+            new { Id = 2, Amount = -500, Type = "Расход", Category = "Еда", Date = DateTime.Now }
+        };
+
+        return Ok(transactions);
+    }
+}
