3857982e8681b36d0f1d0471a50d237a7d932645
Nikody456 <antonnik00@mail.ru>
2025-03-29 03:20:19 +0300

    add crud tests

--- FinanceTracker.API/obj/Debug/net9.0/FinanceTracker.API.AssemblyInfo.cs
+++ FinanceTracker.API/obj/Debug/net9.0/FinanceTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac68a11ef157c66d092b2fdc43bd3cb08b4d5e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0005e1be7be0d7148c2114abe37600668bea46")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
